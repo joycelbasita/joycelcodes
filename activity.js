d99083685dcbbb5284ae3b94401f771f89f2ceb7
joycelbasita <joycelbasita@gmail.com>
2026-09-20 22:00:51 +0800

    Create activity.js

---
+++ activity.js
@@ -0,0 +1,14 @@
+// Simple Shopping Total
+
+let item1 = 50;
+let item2 = 100;
+let item3 = 75;
+
+let total = item1 + item2 + item3;
+
+console.log("Shopping Cart");
+console.log("Item 1: ₱" + item1);
+console.log("Item 2: ₱" + item2);
+console.log("Item 3: ₱" + item3);
+console.log("----------------");
+console.log("Total: ₱" + total);
